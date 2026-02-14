7dbbc3dc9247107ddac001aeeee7b3bc62c1e5a6
cmouli692 <cmouli692@gmail.com>
2026-02-14 18:31:05 +0530

    fix routing and case-sensitive imports for production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Projects from "./pages/Projects";
 import Tasks from "./pages/Tasks";
 import Settings from "./pages/Settings";
