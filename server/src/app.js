deaee91d919dae02bed67cb95b15676d17b9938d
cmouli692 <cmouli692@gmail.com>
2026-02-14 23:39:02 +0530

    added client local and prodution url to server.

--- server/src/app.js
+++ server/src/app.js
@@ -25,7 +25,18 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin:config.clientUrl || "http://localhost:5173",
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        config.clientUrl ,
+        "http://localhost:5173",
+      ];
+      if(!origin || allowedOrigins.includes(origin)){
+        callback(null, true);
+      }
+      else{
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods : ["GET" , "POST" , "PUT" , "DELETE" ],
   })
